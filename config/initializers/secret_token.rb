731f97303c60dde43de339bec79d7ae26a79a31c
vplrt <djangositedev@gmail.com>
2014-11-03 12:17:03 +0400

    Initialize repository 2 try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '8ea4cacfcfece6ab32fb5686373f1103ff03ba015db2081b3d933c873a50c4141f39542ed6e6bfa882b6ab02985d7f1670c376e7cd154453ae68f2325eac3564'
