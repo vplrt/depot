40ed704ac84385bceca7a811e9c309a6bc8e3e86
vplrt <djangositedev@gmail.com>
2014-11-18 22:04:49 +0400

    Chapter 10 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
+
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   resources :products
   root to: 'store#index', as: 'store'
 
